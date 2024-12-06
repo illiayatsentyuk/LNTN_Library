5bb1dbd569fb8f71c55b39e860a07133dc01471f
ksushaluneva <99076279+ksushaluneva@users.noreply.github.com>
2024-12-06 21:17:01 +0200

    commit folders

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// mongoDb
+
+app.listen(3001);
+console.log('trttr')
