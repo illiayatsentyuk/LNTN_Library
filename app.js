fef6addb25e604b020995b051f1d4e13a7232931
illiayatsentyuk <illiayatsentyuk@gmail.com>
2024-12-16 21:25:38 +0200

    add login page

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const path = require('path');
+const path = require("path");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.get("/login", (req, res) => {
+  res.render("login-page", {
+    title: "123",
+  });
+});
 
 // mongoDb
 
 app.listen(3001);
-console.log('trttr')
+console.log("trttr");
